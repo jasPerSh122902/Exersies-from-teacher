1141b15c01ea12c9851ead208a9bf44ddc29cd83
jasper <s218005@students.aie.edu>
2021-09-20 09:35:51 -0500

    Made the files.

---
+++ Exersises from teacher/Exersises from teacher/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exersises_from_teacher
+{
+    class Game
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
