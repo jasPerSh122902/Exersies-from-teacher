636dcb502ac1018df339209cbebc8da7f56c4415
jasper <s218005@students.aie.edu>
2021-10-04 14:21:00 -0500

    thiis the newArray.GetLength

---
+++ This is the third project/tik-tak-to/tik-tak-to/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace tik_tak_to
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array2DitExample = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            //you can not have the new int for the 2 example because it will go off of the amound yo make.
+            int[,] array2DitExample2 = { { 1, 2, 3 },
+                                         { 4, 5, 6 },
+                                         { 7, 8, 9 },
+                                         { 10, 11, 12 } };
+            Math(array2DitExample2);
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j <3; j++)
+                {
+                    Console.WriteLine(array2DitExample[i, j]);
+                }
+            }
+
+            void Math(int[,] newArray )
+            {
+                for (int i = 0; i < newArray.GetLength(0); i++)
+                {
+                    int sum = 0;
+                    for (int j = 0; i < newArray.GetLength(1); i++)
+                    {
+                        sum += newArray[i, j];
+                      
+                    }
+                    Console.WriteLine(sum);
+
+                }
+                
+            }
+
+        }
+    }
+}
